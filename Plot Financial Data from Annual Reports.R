47ed61483c26a36ba9e2f9ebc2620c7c220a21a4
hung991104 <hung991104s110052@gmail.com>
2021-05-20 00:57:37 +0800

    Plot multiple margins in the same graph

--- Plot Financial Data from Annual Reports.R
+++ Plot Financial Data from Annual Reports.R
@@ -38,10 +38,51 @@ DataSummary = DataSummary %>%
 
 # Plot Important Metrics --------------------------------------------------
 # Add columns for gross margin, operating margin
-DataSummary %>% 
+DataSummary_Margin = DataSummary %>% 
   mutate(Gross_Margin = gross_profit/revenue,
          Operating_Margin = operating_income/revenue,
          Net_Margin = net_income/revenue,
-         Payback_Ratio = (-1) * (repurchase + dividends)/net_income)
+         Payback_Ratio = (-1) * (repurchase + dividends)/net_income,
+         date = lubridate::ymd(paste0(year, "1231")))
+
+# Plot multiple margins
+DataSummary_Margin %>%
+  select(date, ends_with("Margin")) %>%
+  pivot_longer(c(-1), names_to = "margin", values_to = "value") %>%
+  ggplot(aes(date, value, group = margin, color = margin)) +
+  geom_line() +
+  scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, by = 0.1)) +
+  labs(y = "Margin (%)", title = "Margins by fiscal years") +
+  theme(plot.title = element_text(hjust = 0.5))     # title in the middle
+
+# Net margin is more volatile as it can be largely affected by non-operating income/expense
+# Better use Net Operating Profit After Tax/ Normalized earning for forcast
 
 
+# Regression on Revenue Growth --------------------------------------------
+DataSummary %>%
+  select(year, revenue) %>%
+  ggplot(aes(year, revenue)) + 
+  geom_point() +
+  scale_x_continuous(breaks = seq(2011, 2020, by = 1)) +
+  labs(x = "fiscal year", title = "Revenue From 2011 to 2020") +
+  theme(plot.title = element_text(hjust = 0.5))     # title in the middle
+
+# fit log(revenue) against year
+Revenue_ln_lmfit = lm(log(revenue) ~ year, data = DataSummary)
+summary(Revenue_ln_lmfit)
+plot(DataSummary$year, log(DataSummary$revenue))
+abline(Revenue_ln_lmfit)
+
+# Estimated Revenue growth = coefficient of year
+coefficients(Revenue_ln_lmfit)
+print(paste0("Estimated Revenue Growth = ", round(100 * coefficients(Revenue_ln_lmfit)[[2]], 2), "%"))
+summary(Revenue_ln_lmfit)$adj.r.squared
+print(paste0("Adj. R squrared = ", round(summary(Revenue_ln_lmfit)$adj.r.squared,4)))
+
+# Plot summary of linear regression fit
+par(mfrow = c(2,2))
+plot(Revenue_ln_lmfit)   
+dev.off()    # reset the graph setting
+
+# Note that the growth rate in the past is not a good indicator of growth in the future!
